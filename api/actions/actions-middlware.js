92009dabdad754d0e8cea7d5ef339b58e8b8fd77
Fatimah Ismael <fatimahismael@yahoo.com>
2023-03-31 10:11:34 -0700

    dendencies, logger and scripts

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,11 @@
 // add middlewares here related to actions
+function logger(req, res, next){
+    const timestamp = new Date().toString();
+    const method= req.method;
+    const url = req.originalUrl;
+    console.log(`${timestamp} ${method} ${url}`)
+    next()
+}
+module.exports = {
+    logger, 
+}
--- api/server.js
+++ api/server.js
@@ -1,5 +1,20 @@
 const express = require('express');
+const { logger } = require('./actions/actions-middlware')
 const server = express();
+
+server.use(express.json())
+server.use(logger)
+
+
+/*server.get('/api/projects', (req, res)=>{
+    res.json({ message: 'API is working'})
+})
+
+server.get('/', (req, res)=>{
+    res.send('<h1>Welcome to the sprint challenge</h1>')
+})
+*/
+// create server.use error code here
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
