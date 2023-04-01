c40e80522b18961031391cf069b22b34c55cf98f
Fatimah Ismael <fatimahismael@yahoo.com>
2023-04-01 15:08:21 -0700

    wip

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,63 @@
 // add middlewares here related to projects
+const Projects = require('./projects-model')
+
+async function validateId (req, res, next){
+    try{
+        const project = await Projects.get(req.params.id)
+        if(!project){
+            res.status(404).json({
+                message:"project not found"
+            })
+        } else{
+            req.project = project
+            next()
+        }
+    } catch(err){
+        res.status(500).json({
+            message:'unable to find project'
+        })
+    }
+}
+
+function validateProject(req, res, next){
+    try{
+        const { name, description }= req.body
+        if(!name || !description){
+            res.status(400).json({
+                message: 'missing info'
+            })
+        } else{
+            next()
+        }
+    } catch(err){
+        res.status(500).json({
+            message:"Error cannot update project"
+        })
+    }
+}
+
+async function validateProjectInfo(req, res, next){
+    const{name, description, notes, completed }= req.body;
+    if(req.method==='PUT'){
+        if(!name|| !description || !(completed ===false || completed === true)){
+            res.status(400).json({ message: 'name, description, notes, and completed are needed'})
+        } else{
+            next()
+        }
+        } else{
+            if(!name || !description){
+                res.status(400).json({message: 'name, description needed'})
+            } else{
+                next()
+            }
+        }
+    
+}
+
+
+
+module.exports ={
+    validateId,
+    validateProject,
+    validateProjectInfo
+}
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,6 +1,7 @@
 // Write your "projects" router here!
 // use endpoints get, put post etc
 const express = require('express')
+const { validateId, validateProject, validateProjectInfo } = require('./projects-middleware')
 
 const Projects= require('./projects-model')
 const router=express.Router();
@@ -11,10 +12,21 @@ router.get('/', (req, res, next)=>{
     .then(projects=>{
         res.status(200).json(projects)
     })
-
     .catch(next)
-    
 })
+router.get('/:id', validateId, (req, res)=>{
+    res.json(req.project)
+})
+router.post('/', validateProject, (req, res, next)=>{
+    Projects.insert(req.body)
+    .then(newProject=>{
+        res.status(201).json(newProject)
+    })
+    .catch(next)
+})
+
+
+
 
 
 module.exports= router
