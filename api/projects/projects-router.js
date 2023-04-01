d455040c42101dcceb946a7555ed23fa08951976
Fatimah Ismael <fatimahismael@yahoo.com>
2023-04-01 15:27:54 -0700

    wip

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -24,8 +24,31 @@ router.post('/', validateProject, (req, res, next)=>{
     })
     .catch(next)
 })
+router.put('/:id', validateProject, validateProjectInfo, async (req, res, next)=>{
+    try{
+        const updatedProjects = await Projects.update(req.params.id, req.body)
+        res.status(200).json(updatedProjects)
+    }
+   catch(err){
+    next(err)
+   }
+    
+})
 
-
+router.delete('/:id', async (req, res, next)=>{
+    try{
+        const deleted = await Projects.remove(req.params.id)
+        if(!deleted){
+            res.status(404).json()
+        } else{
+            res.json({
+                message: 'project deleted'
+            })
+        }
+    } catch{err}{
+        res.status(500).json({message:'Error Error'})
+    }
+})
 
 
 
